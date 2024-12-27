7053f36de806d359662354b02448cbb635401b02
SATHVIK SVS <svssathvik77@gmail.com>
2024-12-27 11:54:05 +0530

    add default first api expansion

--- docs/developers/api/swagger/SwaggerComponent.js
+++ docs/developers/api/swagger/SwaggerComponent.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useEffect, useCallback } from 'react';
 import { SwaggerDoc } from './Swagger';
 
-const SwaggerComponent = ({ url }) => (
-    <SwaggerDoc
-      url={url}
-      docExpansion='none'
-    />
-    );
+const SwaggerComponent = ({ url }) => {
+  const expandFirstTag = ()=>{
+    const firstButton = document.getElementsByClassName("expand-operation")[0];
+    if(firstButton){
+      firstButton.click();
+    }
+  }
+
+  return (
+    <div className="swagger-wrapper">
+      <SwaggerDoc
+        url={url}
+        docExpansion="none"
+        onComplete={expandFirstTag}
+      />
+    </div>
+  );
+};
 
 export default SwaggerComponent;
